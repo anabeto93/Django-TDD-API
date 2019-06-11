59d13559fac759c64827923b314b81464d7426a7
Richard Anabeto Opoku <anabeto93@gmail.com>
2019-06-11 22:08:40 +0000

    added the custom user model as well as creating the superuser functionality which passed

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -19,7 +19,7 @@ class ModelTests(TestCase):
 
     def test_new_user_email_normalized(self):
         '''Test the email of a new user is normalized.'''
-        email = 'test1@AdmiN.com' 
+        email = 'test1@AdmiN.com'
         user = get_user_model().objects.create_user(email, 'pass1234')
 
         self.assertEqual(user.email, email.lower())
@@ -28,3 +28,13 @@ class ModelTests(TestCase):
         '''Test creating a new user without an email will fail'''
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'pass1234')
+
+    def test_super_user_is_created_successfully(self):
+        '''Test creating a new superuser works'''
+
+        user = get_user_model().objects.create_superuser(
+            'you@mybuz.com', 'haha1234'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
