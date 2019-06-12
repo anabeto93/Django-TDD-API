80df067041d830a1c16302a389ecf991df210e8e
Richard Anabeto Opoku <anabeto93@gmail.com>
2019-06-12 22:34:56 +0000

    passed the management command 'wait_for_db' and all linting requirements

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,22 @@
+import time
+
+from django.db import connections
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    '''Command to pause execution until the database is ready.'''
+
+    def handle(self, *args, **kwargs):
+        self.stdout.write('Waiting for database...')
+        db_conn = None
+
+        while not db_conn:
+            try:
+                db_conn = connections['default']
+            except OperationalError:
+                self.stdout.write('Database unavailable, waiting 1 second...')
+                time.sleep(1)  # sleep for 1000ms
+
+        self.stdout.write(self.style.SUCCESS('Database available!'))
