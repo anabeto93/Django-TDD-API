51fd8218748e19d40d978403cfa6c7e2708e9382
Richard Anabeto Opoku <anabeto93@gmail.com>
2019-06-14 09:41:47 +0000

    tests for the tag str or unicode has passed

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,11 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
+
+
+def sample_user(email='test_user@admin.com', password='testpass123'):
+    '''Create a sampler User account.'''
+    return get_user_model().objects.create_user(email, password)
 
 
 class ModelTests(TestCase):
@@ -38,3 +44,12 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_string(self):
+        '''Test that tag string works.'''
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Macho'
+        )
+
+        self.assertEqual(str(tag), tag.name)
